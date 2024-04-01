e875239acd666b1dc71dc31b6b3d5554bd494609
Md. Shakil <shakil.eub.cse@gmail.com>
2024-04-01 19:54:33 +0600

    Portfolio Project Start

---
+++ PortfolioAssignment/portfolioProject/portfolioProject/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from portfolioProject.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',homePage,name="homePage"),
+]
