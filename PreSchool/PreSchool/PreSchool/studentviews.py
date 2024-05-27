5614f060472da65d8f5a38e44c3c9d23222381a2
Md. Shakil <shakil.eub.cse@gmail.com>
2024-05-27 18:39:37 +0600

    Update Student Section

--- PreSchool/PreSchool/PreSchool/studentviews.py
+++ PreSchool/PreSchool/PreSchool/studentviews.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from PreSchoolApp.models import *
+from django.contrib import messages
 
 
 #-----------Student Section--------------------
@@ -8,76 +9,79 @@ def studentDashboard(request):
     return render(request,'students/studentdashboard.html')
 
 def addStudent(request):
+    departmentdata = DepartmentModel.objects.all()
+    context = {
+        'departmentdata':departmentdata,
+    }
     if request.method == 'POST':
         fullname= request.POST.get('fullname')
         username= request.POST.get('username')
         studentid= request.POST.get('studentid')
-        email= request.POST.get('email')
         gender= request.POST.get('gender')
         dateofbirth= request.POST.get('dateofbirth')
-        department= request.POST.get('department')
         religion= request.POST.get('religion')
-        joiningdate= request.POST.get('joiningdate')
         mobile= request.POST.get('mobile')
+        email= request.POST.get('email')
         password= request.POST.get('password')
-        confirmpassword= request.POST.get('confirmpassword')
-        section= request.POST.get('section')
         studentImage= request.FILES.get('studentImage')
+        presentaddress= request.POST.get('presentaddress')
+        permanentaddress= request.POST.get('permanentaddress')
+        
+        department= request.POST.get('department')
+        section= request.POST.get('section')
+        startsession= request.POST.get('startsession')
+        endsession= request.POST.get('endsession')
         
         fathername= request.POST.get('fathername')
         fatheroccupation= request.POST.get('fatheroccupation')
         fathermobile= request.POST.get('fathermobile')
-        fatheremail= request.POST.get('fatheremail')
         
         mothername= request.POST.get('mothername')
         motheroccupation= request.POST.get('motheroccupation')
         mothermobile= request.POST.get('mothermobile')
-        motheremail= request.POST.get('motheremail')
-        
-        presentaddress= request.POST.get('presentaddress')
-        permanentaddress= request.POST.get('permanentaddress')
-        
-        current_user = request.user
-        if password == confirmpassword:
-            user = customeUser.objects.create_user(username=username, password=password)
-            user.email = email
-            user.UserType = '3'
-            user.ProfilePic = studentImage
+
+        if customeUser.objects.filter(username=username).exists():
+            messages.warning(request,'User already exist.')
+        else:            
+            studentuser = customeUser.objects.create_user(
+                username=username,
+                password=password,
+                email = email,
+                UserType = '3',
+                )
+            studentuser.save()
             
             studentData = StudentModel(
+                user=studentuser,
                 FullName=fullname,
-                Username=username,
                 StudentID=studentid,
                 Gender=gender,
                 DateOB=dateofbirth,
-                Department=department,
                 Religion=religion,
-                JoiningDate=joiningdate,
                 Mobile=mobile,
-                Password=password,
+                StudentImage=studentImage,
+                PresentAddress=presentaddress,
+                PermanentAddress=permanentaddress,
+                
+                Department=department,
+                StartSession=startsession,
+                EndSession=endsession,
+                
                 Section=section,
                 
                 FatherName=fathername,
                 FatherOccupation=fatheroccupation,
                 FatherMobile=fathermobile,
-                FatherEmail=fatheremail,
                 
                 MotherName=mothername,
                 MotherOccupation=motheroccupation,
                 MotherMobile=mothermobile,
-                MotherEmail=motheremail,
-                
-                PresentAddress=presentaddress,
-                PermanentAddress=permanentaddress,
-                Added_by=current_user,
             )
-            
-            user.save()
             studentData.save()
-            return redirect('studentList')            
-    
-    
-    return render(request,'students/addstudent.html')
+            messages.success(request,'Successfully Created.')
+            return redirect('studentList') 
+        
+    return render(request,'students/addstudent.html',context)
 
 def editStudent(request):
     
@@ -96,19 +100,21 @@ def studentList(request):
 
 def studentDetails(request,myid):
     studentinfo = StudentModel.objects.get(id=myid)
-    userdata=customeUser.objects.get(username=studentinfo.Username)
+    userdata=customeUser.objects.get(username=studentinfo.user)
+    departmentdata = DepartmentModel.objects.all()
     
     stddict = {
         'studentinfo':studentinfo,
         'userdata':userdata,
+        'departmentdata':departmentdata,
+        
     }
     
     return render(request,'students/studentdetails.html',stddict)
 
 def deleteStudent(request,myid):
     deletedata = StudentModel.objects.get(id=myid)
-    customuser = deletedata.Username
-    print("MY data: ",customuser)
+    customuser = deletedata.user
     userdata=customeUser.objects.get(username=customuser)
     deletedata.delete()
     userdata.delete()
